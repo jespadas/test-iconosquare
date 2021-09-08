12337f5ae53b0f5c6bac83dec62435a9bd133f4f
jespadas <julio.espadas@gmail.com>
2021-09-08 14:32:56 +0200

    first code for read component

---
+++ src/components/Read.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const Read = () => {
+
+    const [apiData, setApiData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://613885d5163b560017039f42.mockapi.io/JSONPlaceholder`)
+            .then((response) => {
+                setApiData(response.data);
+            });
+    }, []);
+
+    const setData = (data) => {
+        let { id, name, lastName, checkbox } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('First Name', name);
+        localStorage.setItem('Last Name', lastName);
+        localStorage.setItem('Checkbox Value', checkbox);
+    };
+
+    return (
+        <div>
+            <Table singleLine>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>First Name</Table.HeaderCell>
+                        <Table.HeaderCell>Last Name</Table.HeaderCell>
+                        <Table.HeaderCell>Checked</Table.HeaderCell>
+                        <Table.HeaderCell>Update</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {apiData.map((data) => {
+                        return (
+                            <Table.Row key={data.id}>
+                                <Table.Cell>{data.name}</Table.Cell>
+                                <Table.Cell>{data.lastName}</Table.Cell>
+                                <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+                                <Link to='/update'>
+                                    <Table.Cell><Button onClick={() => setData(data)}>Update</Button></Table.Cell>
+                                </Link>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+
+            </Table>
+
+        </div>
+    )
+}
+
+export default Read;
