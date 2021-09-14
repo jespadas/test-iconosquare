a37e285313f141fa27f4a571874077fa95c92ace
jespadas <julio.espadas@gmail.com>
2021-09-14 13:56:30 +0200

    posts list and new posts list done

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -10,161 +10,211 @@ const PostsList = () => {
     const userIdPassed = localStorage.getItem('userId');
     const userNamePassed = localStorage.getItem('userName');
 
-    const [open, setOpen] = useState(false)
-
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [posts, setPosts] = useState([]);
     const [newPost, setNewPost] = useState([]);
-    const [postContent] = useState('');
+    const [values, setValues] = useState([]);
 
-    const getApiData = () => {
-        const url = 'https://613885d5163b560017039f42.mockapi.io/api/v1/users/' + JSON.parse(userIdPassed) + '/posts';
-        axios.get(url)
-            .then(response => {
-                setPosts(response.data);
-                console.log(posts);
-            });
-    };
 
     useEffect(() => {
 
         const url = 'https://613885d5163b560017039f42.mockapi.io/api/v1/users/' + JSON.parse(userIdPassed) + '/posts';
         axios.get(url)
             .then(response => {
-                setPosts(response.data);
+                setPosts([...response.data]);
             });
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const postData = () => {
-        const url = 'https://613885d5163b560017039f42.mockapi.io/api/v1/newPosts';
-        axios.post(url, {
-            postTitle: newPost,
-            postContent
-        }).then(
-            setOpen(!open),
-            setPosts(posts)
-        )
+    /*     const postData = () => {
+            const url = 'https://613885d5163b560017039f42.mockapi.io/api/v1/newPosts';
+            axios.post(url, {
+                values
+            }).then(
+                setOpen(!open),
+            )
+        }; */
 
+    const createPost = () => {
+        setNewPost(newPost => [...newPost, values]);
+        setOpen(!open)
+        console.log(newPost, 'createpost');
+    }
+
+    const handleInputChange = ({ target }) => {
+        const { name, value } = target;
+        const postId = Math.floor(Math.random() * 100);
+        setValues({ ...values, [name]: value, postId });
     };
 
     return (
         <div className="posts-container">
             <List selection verticalAlign='middle' >
-                <Grid columns={3} divided stackable >
+                <Grid columns={3} divided container stackable>
                     <h1>Posts list for {userNamePassed}</h1>
-                    <Grid.Row>
-                        <Grid.Column>
+                    {loading ?
+                        <h1>LOADNG</h1>
+                        :
+                        <Grid.Row>
+                            <Grid.Column mobile={16} tablet={8} computer={4} key={1}>
+                                <Modal
+                                    onClose={() => setOpen(false)}
+                                    onOpen={() => setOpen(true)}
+                                    open={open}
+                                    trigger={
+                                        <Button className="create-button">
+                                            <span>+</span><br /><span>Create a new post</span>
+                                        </Button>
+                                    }
+                                >
+                                    <Modal.Header>
+                                        <span>Create a new post: </span>
+                                    </Modal.Header>
+                                    <Modal.Content>
+                                        <Modal.Description>
+                                            <Form>
+                                                <Form.Field>
+                                                    <label>Title</label>
+                                                    <input
+                                                        placeholder='Your comment title...'
+                                                        autoComplete='off'
+                                                        onChange={(e) => handleInputChange(e)}
+                                                        name='postTitle'
+                                                        autoFocus
+                                                    />
+                                                </Form.Field>
+                                                <Form.Field>
+                                                    <label>Post</label>
+                                                    <input
+                                                        placeholder='Your comment...'
+                                                        autoComplete='off'
+                                                        onChange={(e) => handleInputChange(e)}
+                                                        name='postContent'
 
-                            <Modal
-                                onClose={() => setOpen(false)}
-                                onOpen={() => setOpen(true)}
-                                open={open}
-                                trigger={
-                                    <Button className="create-button">
-                                        <span>+</span><br /><span>Create a new post</span>
-                                    </Button>
-                                }
-                            >
-                                <Modal.Header>
-                                    <span>Create a new post :</span>
-                                </Modal.Header>
-                                <Modal.Content>
-                                    <Modal.Description>
-                                        <Form onSubmit={postData}>
-                                            <Form.Field>
-                                                <label>Title</label>
-                                                <input
-                                                    placeholder='Your comment title...'
-                                                    autoComplete='off'
-                                                    onChange={(e) => setNewPost(e.target.value)}
-                                                    name='postTitle'
-                                                />
-                                            </Form.Field>
+                                                    />
+                                                </Form.Field>
+                                                <Modal.Actions>
+                                                    <Button
+                                                        color="blue"
+                                                        onClick={createPost}
+                                                        type='submit'
+                                                    >
+                                                        Create
+                                                    </Button>
+                                                </Modal.Actions>
+                                            </Form>
+                                        </Modal.Description>
+                                    </Modal.Content>
 
-                                            <Form.Field>
-                                                <label>Post</label>
-                                                <input
-                                                    placeholder='Your comment...'
-                                                    autoComplete='off'
-                                                    name='postTitle'
-                                                />
-                                            </Form.Field>
-                                            <Modal.Actions>
-                                                <Button
-                                                    color="blue"
-                                                    onClick={postData}
-                                                    type='submit'
+                                </Modal>
+
+                            </Grid.Column>
+                            {/* NEW POST LIST */}
+                            {
+                                newPost
+                                    ?
+                                    newPost.map(({ postId, postContent, postTitle }) => {
+                                        console.log(postId);
+                                        return (
+                                            <Grid.Column as="h2" mobile={16} tablet={8} computer={4} key={postId}>
+                                                <List selection verticalAlign='middle' className="list-item">
+                                                    <Modal
+                                                        key={postId}
+                                                        onClose={() => setOpen(false)}
+                                                        onOpen={() => setOpen(true)}
+                                                        trigger={
+                                                            <List.Item key={postId}>
+                                                                <List.Content >
+                                                                    <List.Header as="h2">{postTitle}</List.Header>
+                                                                    <h3>{postContent}</h3>
+                                                                </List.Content>
+                                                            </List.Item>}
+                                                        postcontent={postContent}
+                                                    >
+                                                        <Modal.Header>
+                                                            <span>{postTitle}</span>
+                                                        </Modal.Header>
+                                                        <Modal.Content>
+                                                            <Modal.Description>
+                                                                <p>{postContent}</p>
+                                                            </Modal.Description>
+                                                        </Modal.Content>
+                                                    </Modal>
+                                                </List>
+                                            </Grid.Column>
+                                        )
+                                    })
+                                    :
+                                    null
+                            }
+                            {/* API POSTS LIST */}
+                            {
+                                posts.map(({ postTitle, postId, postContent, postsComments }) => {
+                                    return (
+                                        <Grid.Column as="h2" key={postId} mobile={16} tablet={8} computer={4}>
+                                            <List selection verticalAlign='middle' className="list-item">
+                                                <Modal
+                                                    key={postId}
+                                                    onClose={() => setOpen(false)}
+                                                    onOpen={() => setOpen(true)}
+                                                    trigger={
+                                                        <List.Item key={postId} >
+                                                            <List.Content >
+                                                                <List.Header as="h2">{postTitle}</List.Header>
+                                                                <h3>{postContent}</h3>
+                                                            </List.Content>
+                                                        </List.Item>}
+
                                                 >
-                                                    Create
-                                                </Button>
-                                            </Modal.Actions>
-                                        </Form>
-                                    </Modal.Description>
-                                </Modal.Content>
+                                                    <Modal.Header>
+                                                        <span>{postTitle}</span>
+                                                    </Modal.Header>
+                                                    <Modal.Content>
+                                                        <Modal.Description>
+                                                            <p>{postContent}</p>
+                                                            {postsComments !== undefined
+                                                                ?
+                                                                <Comment.Group>
+                                                                    <Header as='h3' dividing>
+                                                                        Comments ({postsComments.length})
+                                                                    </Header>
 
-                            </Modal>
+                                                                    <Comment key={postId}>
+                                                                        <Comment.Avatar src={postsComments[0].commentAvatar} />
+                                                                        <Comment.Content>
+                                                                            <Comment.Author>{postsComments[0].commentAuthor}</Comment.Author>
+                                                                            <Comment.Metadata>
+                                                                                <div>{
+                                                                                    postsComments[0].commentDate}</div>
+                                                                            </Comment.Metadata>
+                                                                            <Comment.Text>{postsComments[0].commentContent}</Comment.Text>
 
-                        </Grid.Column>
-                        {
-                            posts.map((post) => {
-                                return (
-
-                                    <Grid.Column as="h2" key={post.postId} >
-                                        <List selection verticalAlign='middle' className="list-item">
-                                            <Modal
-                                                key={post.postId}
-                                                onClose={() => setOpen(false)}
-                                                onOpen={() => setOpen(true)}
-                                                trigger={
-                                                    <List.Item key={post.postId} >
-                                                        <List.Content >
-                                                            <List.Header as="h2">{post.postTitle}</List.Header>
-                                                            <h3>{post.postContent}</h3>
-                                                        </List.Content>
-                                                    </List.Item>}
-                                                postcontent={post.postContent}
-                                            >
-                                                <Modal.Header>
-                                                    <span>{post.postTitle}</span>
-                                                </Modal.Header>
-                                                <Modal.Content>
-                                                    <Modal.Description>
-                                                        <p>{post.postContent}</p>
-                                                        <Comment.Group>
-                                                            <Header as='h3' dividing>
-                                                                Comments ({post.postsComments.length})
-                                                            </Header>
-
-                                                            <Comment key={post.postId}>
-                                                                <Comment.Avatar src={post.postsComments[0].commentAvatar} />
-                                                                <Comment.Content>
-                                                                    <Comment.Author>{post.postsComments[0].commentAuthor}</Comment.Author>
-                                                                    <Comment.Metadata>
-                                                                        <div>{
-                                                                            post.postsComments[0].commentDate}</div>
-                                                                    </Comment.Metadata>
-                                                                    <Comment.Text>{post.postsComments[0].commentContent}</Comment.Text>
-
-                                                                    {/* TODO make post action for comments
+                                                                            {/* TODO make post action for comments
                                                                     <Comment.Actions>
                                                                         <Comment.Action>Reply</Comment.Action>
                                                                     </Comment.Actions> 
                                                                     */}
-                                                                </Comment.Content>
-                                                            </Comment>
-                                                        </Comment.Group>
-                                                    </Modal.Description>
-                                                </Modal.Content>
-                                            </Modal>
-                                        </List>
-                                    </Grid.Column>
-                                )
-                            })
-                        }
+                                                                        </Comment.Content>
+                                                                    </Comment>
+                                                                </Comment.Group>
+                                                                :
+                                                                null
+                                                            }
+                                                        </Modal.Description>
+                                                    </Modal.Content>
+                                                </Modal>
+                                            </List>
+                                        </Grid.Column>
+                                    )
+                                })
+                            }
 
-                    </Grid.Row>
 
+
+                        </Grid.Row>
+                    }
                 </Grid>
             </List>
 
