76b06b0af1a5bd04b0c66692da668f8f84eb14aa
jespadas <julio.espadas@gmail.com>
2021-09-15 10:13:36 +0200

    docs deleted and refactoring router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import AppHeader from '../components/ui/AppHeader';
-import { CreatePost } from '../components/CreateScreen'
+//import { CreatePost } from '../components/CreateScreen'
 import '../styles.css';
 import UsersScreen from '../components/users/UsersScreen';
 import PostsScreen from '../components/posts/PostsScreen';
@@ -17,7 +17,7 @@ export const AppRouter = () => {
                 <Switch>
                     <Route exact path="/" component={UsersScreen} />
                     <Route exact path="/user/:userId/posts" component={PostsScreen} />
-                    <Route exact path="/user/:userId/create-post" component={CreatePost} />
+                    {/* <Route exact path="/user/:userId/create-post" component={CreatePost} /> */}
                     <Redirect to='/' />
                 </Switch>
             </div>
